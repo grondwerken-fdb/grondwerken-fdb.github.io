7c44b0304f1adcd2ca23adbddb69d72dd5a7d5f6
nmdwolf <nicolas.dewolf@hotmail.com>
2024-02-05 18:01:11 +0100

    update

--- _site/ScanFiles.py
+++ _site/ScanFiles.py
@@ -3,12 +3,14 @@ import os
 select = os.listdir("./assets/images")
 
 for folder in select:
-    if not os.path.exists("./_projects/" + folder + ".md"):
-        with open("./_projects/" + folder + ".md", "w+") as file:
-            data = folder.split("_")
-            file.write("---\n")
-            file.write("layout: project\n")
-            file.write("link: " + folder + "\n")
-            file.write("title: " + data[2].strip() + "\n")
-            file.write("date: " + data[0].strip() + "\n")
-            file.write("---\n")
+    # if not os.path.exists("./_projects/" + folder + ".md"):
+    with open("./_projects/" + folder + ".md", "w+") as file:
+        data = folder.split("_")
+        file.write("---\n")
+        file.write("layout: project\n")
+        file.write("link: " + folder + "\n")
+        file.write("date: " + data[0].strip() + "\n")
+        file.write("location: " + data[1].strip() + "\n")
+        file.write("title: " + data[2].strip() + "\n")
+        file.write("permalink: /projects/" + data[2].strip() + "\n")
+        file.write("---\n")
