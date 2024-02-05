c22954924a2719674dbef2e8fb71b4c9e6e1e6b5
nmdwolf <nicolas.dewolf@hotmail.com>
2024-02-05 17:53:37 +0100

    update

---
+++ _projects/2021-11-05_OUDENAARDE_Putdeksel.md
@@ -0,0 +1,6 @@
+---
+layout: project
+link: 2021-11-05_OUDENAARDE_Putdeksel
+title: Putdeksel
+date: 2021-11-05
+---
--- _site/ScanFiles.py
+++ _site/ScanFiles.py
@@ -1,2 +1,14 @@
-with open("temp.txt", "w+") as file:
-    file.write("test test\r\n")
+import os
+
+select = os.listdir("./assets/images")
+
+for folder in select:
+    if not os.path.exists("./_projects/" + folder + ".md"):
+        with open("./_projects/" + folder + ".md", "w+") as file:
+            data = folder.split("_")
+            file.write("---\n")
+            file.write("layout: project\n")
+            file.write("link: " + folder + "\n")
+            file.write("title: " + data[2].strip() + "\n")
+            file.write("date: " + data[0].strip() + "\n")
+            file.write("---\n")
